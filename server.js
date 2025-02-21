da36d8ae67df1d1990afa02296018d80de1bf483
gilang huda <gilanghuda99@gmail.com>
2025-02-21 20:12:05 +0700

    initt

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv")
 dotenv.config()
 const PORT = process.env.PORT || 3000
 const connectDB = require("./db/connect")
-const {createTable, getUser} = require("./db/sqlFunctions")
+const {getUser} = require("./db/sqlFunctions")
 
 app.use(express.json())
 connectDB()
